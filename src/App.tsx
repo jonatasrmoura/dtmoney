0f27be215a0e31448924444507d986cd2faa7d70
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-03-20 00:00:55 -0300

    feat#Estrutura do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Modal from 'react-modal';
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -22,13 +24,10 @@ export function App() {
     <>
       <Header onOpenNewTransitionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-
-      <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-        </Modal>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
